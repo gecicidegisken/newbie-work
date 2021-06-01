3ad46c8134aba67ed3aa4895cd52cdf5b412bd36
gecicidegisken <hayallerimyaziyor@gmail.com>
2021-06-01 16:14:00 +0300

    kacan buton

---
+++ kacan buton/kacan oyun/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace kacan_oyun
+{
+    partial class beniyakala
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(167, 213);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(77, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "hayır";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.button1_MouseMove);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(204, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "İrfan hoca bize yüksek notlar verecek mi?";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(43, 213);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(77, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "EVET";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // beniyakala
+            // 
+            this.ClientSize = new System.Drawing.Size(312, 299);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "beniyakala";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ kacan buton/kacan oyun/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kacan_oyun
+{
+    public partial class beniyakala : Form
+    {
+        public beniyakala()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_MouseMove(object sender, EventArgs e)
+        {
+
+            Random sayisalla = new Random();
+            int a, b;
+            a = sayisalla.Next(1, 300);
+            b = sayisalla.Next(1, 300);
+
+            {
+                button1.Location = new Point(a, b);
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Teşekkür ederiz :)");
+        }
+    }
+}
+
+
+
+
+
+
